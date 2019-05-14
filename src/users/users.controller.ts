b3507a72dc969c7e3caede5bb37de2d43d8c873c
jason zheng <101104044@nccu.edu.tw>
2019-05-15 00:35:58 +0800

    added user controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+    @Get('users')
+    getAllUsers() { }
+
+    @Get('users/:id')
+    getUser() { }
+
+    @Post('users')
+    addUser() { }
+}
