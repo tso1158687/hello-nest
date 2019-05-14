7d882a35c5bcc6ed3e6f317b6fecd82e581056a5
jason zheng <101104044@nccu.edu.tw>
2019-05-15 00:44:33 +0800

    added users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Request, Response, Param, Next, HttpStatus } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    @Get('users')
-    getAllUsers() { }
+    @Get()
+    // 使用Express的參數
+    getAllUsers( @Request() req, @Response() res, @Next() next) {
+        // 假資料
+        const users = [{ Name: 'Michael', Age: 25 }, {Name: 'Jason', Age: 25}];
+        // 多種HttpStatus可用
+        res.status(HttpStatus.OK).json(users);
+    }
 
-    @Get('users/:id')
-    getUser() { }
+    @Get('/:id')
+    // 使用Express的參數
+    getUser( @Param() params) {
+        return { getUser: params.id };
+    }
 
-    @Post('users')
+    @Post()
     addUser() { }
 }
