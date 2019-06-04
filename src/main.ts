3e9ff460a8a573595af79d747bae9c6f12b18545
jason <jason@wishingsoft.com>
2019-06-04 10:17:17 +0800

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { MiddlewareMiddleware } from './share/middleware.middleware';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.use(MiddlewareMiddleware);
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
